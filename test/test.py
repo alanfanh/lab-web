af2c16a687c3adad425c04b5217164785cbaa1de
Alan Fan <alan.fan.h@qq.com>
2026-04-22 11:35:23 +0800

    将test代码防止test文件夹，并重新优化测试代码

--- test/test.py
+++ test/test.py
@@ -1,13 +1,14 @@
 #-*-coding:utf-8-*-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column,String,Integer,Text,ForeignKey
-from sqlalchemy.orm import sessionmaker, relationship
-engine = create_engine('mysql+mysqldb://root:zlp308@localhost:3306/blog')
-print(engine)
+from sqlalchemy import Column, String, Integer, Text, ForeignKey
+from sqlalchemy.orm import relationship, sessionmaker
+
+# 1.创建基类
 Base = declarative_base()
-#nullable表示列不为空，index=True表示在该列创建索引
-#数据类型：String、Integer、Text、Boolean、SmallInteger、DateTime
+
+# 定义模型
+# nullable表示列不为空，index=True表示在该列创建索引
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer,primary_key=True)
@@ -18,8 +19,8 @@ class User(Base):
     def __repr__(self):
         return "%s(%r)" %(self.__class__.__name__,self.name)
 
-#一对多,用户可以写多篇文章
 class Artical(Base):
+    #一对多,用户可以写多篇文章
     __tablename__ = "articals"
     id = Column(Integer,primary_key=True)
     title = Column(String(255),nullable=False,index=True)
@@ -29,8 +30,24 @@ class Artical(Base):
     def __repr__(self):
        return "%s(%r)" %(self.__class__.__name__,self.title)
 
+# 2.创建数据库连接
+URL = 'mysql+mysqldb://root:test1234@localhost:3306/blog'
+engine = create_engine(URL)
+
+def get_db_session():
+    Session = sessionmaker(bind=engine)
+    return Session()
+
 if __name__ == "__main__":
+    # 物理建表
     Base.metadata.create_all(engine)
-    user = User(name='zhangsan',pwd='zhangsan',email='zhangsan@163.com')
-    session.add(user)
-    session.commit()
+    session = get_db_session()
+    try:
+        user = User(name='zhangsan',pwd='zhangsan',email='zhangsan@163.com')
+        session.add(user)
+        session.commit()
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        session.rollback()
+    finally:
+        session.close()
